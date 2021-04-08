5be3d07fa24596485e9ce0dc5a60594cdf791fca
twesigye10 <twesigye10@gmail.com>
2021-04-08 17:09:46 +0300

    added vectors

--- R/R_basics.R
+++ R/R_basics.R
@@ -1,3 +1,12 @@
 # writing first R script
 my_first_string <- "Hello, World!"
 print(my_first_string)
+
+# data structures
+# vectors
+
+fruits <- c("banana", "apple", "mango")
+print(fruits)
+
+some_numbers <- c(1:10)
+print(some_numbers)
