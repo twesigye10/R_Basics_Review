fa12917ad94bbdf57450eedb802ad8c9999cbde2
twesigye10 <twesigye10@gmail.com>
2021-04-09 17:02:27 +0300

    looked at data structures like lists, vectors, data frames, matrices

--- R/R_basics.R
+++ R/R_basics.R
@@ -14,3 +14,41 @@ print(some_numbers)
 # check data type using class() function
 class(fruits)
 class(some_numbers)
+
+# lists
+my_list <- list(c(2,5,3),21.3,sin)
+my_list
+
+# Matrices
+
+my_matrix  <-  matrix( some_numbers, nrow = 5, ncol = 10, byrow = TRUE)
+my_matrix
+
+# Arrays
+my_array <- array(fruits, dim = c(3,3,4))
+my_array
+
+# Factors
+
+my_colours <- c("Red", "Green", "Red", "Blue", "Blue","Green", "Red", "Blue" )
+# check to see if it is a factor
+is.factor(my_colours)
+
+my_colours_factor <- factor(my_colours)
+my_colours_factor
+# check to see if it is a factor
+is.factor(my_colours_factor)
+
+# Data Frames
+# Data frames are simply tabular data objects. 
+my_data_frame <- data.frame(
+  gender = c("Male", "Male","Female"), 
+  height = c(152, 171.5, 165), 
+  weight = c(81,93, 78),
+  Age = c(42,38,26)
+)
+
+my_data_frame
+
+class(my_data_frame)
+
