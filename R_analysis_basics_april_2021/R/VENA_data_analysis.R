a9f2b97f5bb080dda5ea7b1a16740d5007f8b15b
Anthony Twesigye <twesigye10@gmail.com>
2021-08-02 17:44:41 +0300

    child_harsh_PSN

--- R_analysis_basics_april_2021/R/VENA_data_analysis.R
+++ R_analysis_basics_april_2021/R/VENA_data_analysis.R
@@ -21,3 +21,15 @@ settlement_mean_hh_size <- df_data %>%
       round(2)
   )
 
+
+df_child_labour <- df_data %>% 
+  select(contains("child_harsh_PSN"), Settlement)
+
+# summarise
+df_ch <- df_child_labour %>% 
+  group_by(Settlement) %>%
+  mutate(across(where(is.character), as.numeric)) %>% 
+  rowwise() %>% 
+  mutate(total_psn = sum(child_harsh_PSN_1_labor_heavy_loads: child_harsh_PSN_4_labor_exposure_to_extreme_heat, na.rm = TRUE)) %>% 
+  summarise(total_psn_per_settlement = sum(total_psn, na.rm = TRUE))
+
