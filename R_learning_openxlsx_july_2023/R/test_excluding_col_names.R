63bcbab9019c1e4cdc364079c7ae4fe29f62b828
Anthony Twesigye <twesigye10@gmail.com>
2024-02-20 14:41:33 +0300

    clean up

--- R_learning_openxlsx_july_2023/R/test_excluding_col_names.R
+++ R_learning_openxlsx_july_2023/R/test_excluding_col_names.R
@@ -8,12 +8,6 @@ df_to_format <- data.frame("Date" = Sys.Date()-0:4,
                            "Percentage" = seq(-1, 1, length.out=5),
                            "TinyNumber" = runif(5) / 1E9, stringsAsFactors = FALSE)
 
-# class(df_to_format$Currency) <- "currency"
-# class(df_to_format$Accounting) <- "accounting"
-# class(df_to_format$hLink) <- "hyperlink"
-# class(df_to_format$Percentage) <- "percentage"
-# class(df_to_format$TinyNumber) <- "scientific"
-
 ## Formatting can be applied simply through the write functions
 ## global options can be set to further simplify things
 options("openxlsx.borderStyle" = "thin")
@@ -31,11 +25,6 @@ wb_format <- createWorkbook()
 
 addWorksheet(wb_format, sheetName = "writeDataTable")
 
-# loading styles from another file
-
-# template_file <- loadWorkbook("support_files/MSNA2022_Livelihood_tables.xlsx")
-# 
-# template_styles <- getStyles(template_file)
 
 hs1_formatting <- createStyle(fgFill = "#EE5859", halign = "CENTER", textDecoration = "Bold", border = "Bottom", fontColour = "white")
 
@@ -46,10 +35,7 @@ writeData(wb = wb_format, sheet = "writeDataTable", x = "Adding a heading", star
 addStyle(wb = wb_format, sheet = "writeDataTable", hs2_formatting, rows = 1, cols = 2:7, gridExpand = TRUE)
 
 writeData(wb = wb_format, sheet = "writeDataTable", x = df_to_format, startRow = 4, startCol = 2, 
-               # tableStyle = "TableStyleLight9", headerStyle = hs1_formatting, 
-               colNames = FALSE)
-
-# openXL(wb_format) ## opens a temp version
+          colNames = FALSE)
 
 saveWorkbook(wb_format, "outputs/testing_openxls_v3.xlsx", overwrite = TRUE)
 openXL(file = paste0("outputs/testing_openxls_v3.xlsx"))
